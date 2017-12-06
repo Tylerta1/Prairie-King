46028b08c95c7c29eb9c933a1617023b81430a51
Tyler Ta <tyler.ta@utexas.edu>
2017-12-06 01:26:26 -0600

    Pics

--- spaceinvaders.d
+++ spaceinvaders.d
@@ -1,5 +1,5 @@
 .\spaceinvaders.o: SpaceInvaders.c
-.\spaceinvaders.o: C:\Users\Alex\Desktop\EE 319K - Ta\ARM\ARMCC\bin\..\include\stdint.h
+.\spaceinvaders.o: C:\Users\Tyler Ta\Desktop\Keil_Ta\ARM\ARMCC\bin\..\include\stdint.h
 .\spaceinvaders.o: tm4c123gh6pm.h
 .\spaceinvaders.o: ST7735.h
 .\spaceinvaders.o: Random.h
@@ -9,3 +9,4 @@
 .\spaceinvaders.o: Sprite.h
 .\spaceinvaders.o: Timer1.h
 .\spaceinvaders.o: Timer0.h
+.\spaceinvaders.o: Pictures.h
